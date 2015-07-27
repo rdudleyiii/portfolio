d2582a965b14a046d79aa5d38d362dd33abdc9a5
rdudleyiii <dudley9@gmail.com>
2015-07-27 18:36:23 -0500

    updates to skills

--- robert-portfolio/robert-portfolio.js
+++ robert-portfolio/robert-portfolio.js
@@ -1,25 +1,48 @@
 if (Meteor.isClient) {
-	Template.index.events = {
-		'click .masthead-nav li, click .masthead-brand a, click .toTop' : function(e){
-			var $this = $(e.target);
+	// new Chartist.Pie('.ct-chart', {
+	//     series: [70, 30]
+	// }, {
+	//     donut: true,
+	//     donutWidth: 60,
+	//     startAngle: 270,
+	//     total: 200,
+	//     showLabel: false
+	// });
 
-			if (!$this.hasClass('toTop')) {
-				$('.active').removeClass('active');
-				$this.parent().addClass('active');
-			}
+    Template.index.events = {
+        'click .lead-button a': function(e) {
+            var $this = $(e.target);
+            var scrollspot = $('.marketing').offset().top - $('.masthead').height();
 
-			$('html, body').scrollTo(("." + $this.attr('data-scroller')), 500);
-		}
-	}
+            $('html, body').animate({
+            	scrollTop: scrollspot
+            }, 500);
 
-	Template.index.rendered = function(){
-		new WOW().init()
-	} 
-  		
+        },
+
+        'click .masthead-nav li, click .masthead-brand a, click .toTop': function(e) {
+            var $this = $(e.target);
+            var scrollSpot = $("." + $this.attr('data-scroller')).offset().top - $('.masthead').height();
+
+            if (!$this.hasClass('toTop')) {
+                $('.active').removeClass('active');
+                $this.parent().addClass('active');
+            }
+
+            $('html, body').animate({
+            	scrollTop: scrollspot
+            }, 500);
+        }
+    }
+
+    Template.index.rendered = function() {
+        new WOW().init()
+    }
+    $('html').attr('lang', 'en').attr('xml:lang', 'en').attr('xmlns', 'http://www.w3.org/1999/xhtml');
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
