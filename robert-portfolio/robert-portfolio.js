052c441f5bfacaafefa472d276c9324ad8de4e10
Robert Dudley <dudley9@gmail.com>
2015-07-29 20:22:03 +0000

    rename templaet

--- robert-portfolio/robert-portfolio.js
+++ robert-portfolio/robert-portfolio.js
@@ -1,15 +1,7 @@
 if (Meteor.isClient) {
-	// new Chartist.Pie('.ct-chart', {
-	//     series: [70, 30]
-	// }, {
-	//     donut: true,
-	//     donutWidth: 60,
-	//     startAngle: 270,
-	//     total: 200,
-	//     showLabel: false
-	// });
-
+	
     Template.index.events = {
+        
         'click .lead-button a': function(e) {
             var $this = $(e.target);
             var scrollspot = $('.marketing').offset().top - $('.masthead').height();
@@ -33,11 +25,12 @@ if (Meteor.isClient) {
             	scrollTop: scrollspot
             }, 500);
         }
-    }
+    };
 
     Template.index.rendered = function() {
-        new WOW().init()
-    }
+        new WOW().init();
+    };
+    
     $('html').attr('lang', 'en').attr('xml:lang', 'en').attr('xmlns', 'http://www.w3.org/1999/xhtml');
 }
 
